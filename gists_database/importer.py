54f377544e92c4c87aeb3adc9e9d361391b130b4
basha110209 <bashayr1029@gmail.com>
2024-11-28 18:30:16 +0300

    Update importer.py

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,17 @@
 import requests
+import sqlite3
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = f"https://api.github.com/users/{username}/gists"
+    response = requests.get(url)
+    gists = response.json()
+
+    cursor = db.cursor()
+    for gist in gists:
+        cursor.execute("""
+            INSERT INTO gists (github_id, description, created_at, updated_at, public)
+            VALUES (?, ?, ?, ?, ?)
+        """, (gist['id'], gist['description'], gist['created_at'], gist['updated_at'], gist['public']))
+    
+    if commit:
+        db.commit()
